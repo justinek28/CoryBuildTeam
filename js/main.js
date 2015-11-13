25c582f5209ed41f00311afb5c30a500ebd0faa5
justinek28 <henderson.justine@gmail.com>
2015-11-12 17:47:33 -0700

    added main.js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(function() {
+  $('.submit-form').click(submitForm);
+});
+
+function submitForm() {
+  if ($('#human').val() == 5) {
+    var email = {
+      key: "DV6iDBf0ZF_bcscqfJalXw",
+      message: {
+        to: [
+          {
+            email: "hello@justinehenderson.com"
+          }
+        ],
+        from_email: $('#email').val(),
+        from_name: $('#name').val(),
+        subject: $('#name').val() + ' sent you an email',
+        html: $("#message").val(),
+        autotext: "true"
+      }
+    };
+    $.ajax({
+      type: "POST",
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: email,
+      success: function(msg){
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+        $('#human').val('');
+        alert('Email sent successfuly!');
+      }, error: function(XMLHttpRequest, textStatus, errorThrown) {
+        alert('Somethin went wrong! Try again.');
+      }
+    });
+  } else {
+    alert('Are you sure you\'re human?');
+  }
+}
